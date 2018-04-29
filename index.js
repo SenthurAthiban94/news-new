874f4a0fca532d9d9cd21e5a500ca3de1e335d73
senthur <senthur@financebuddha.com>
2018-04-29 15:02:16 +0530

    React App

--- index.js
+++
@@ -1,5 +0,0 @@
-var trends = require('node-google-search-trends');
-trends('United States', 10, function(err, data) {
-    if (err) return console.err(err);
-    console.log(JSON.stringify(data, null, 2));  // Pretty prints JSON 'data'
-}); 
