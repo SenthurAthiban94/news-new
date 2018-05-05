890be8b0657d8c0d35c7d8a6ea3ee1835405ef48
senthur <senthur@financebuddha.com>
2018-05-06 04:23:52 +0530

    UI Changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import Ad from './Ad';
 import Header from './Header';
+import Ad from './Ad';
+import Search from './Search';
+import Blogcontent from './Blogcontent';
 import './../styles/css/Main.css';
 
 class App extends Component {
@@ -9,72 +11,31 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="page_background">
-        <Header logo_url={"./assets/images/logo.png"} />
-        {/* <Ad position={"TOP"} content={""}/> */}
+      <div>
         <div className="container-fluid">
           <div className='row'>
+            <div className="sticky header">
+              <Header logo_url={"./assets/images/logo.png"} />
+            </div>
+          </div>
+          <div className='row'>
             <div className="col-sm-3 col-xs-3 col-md-3 col-lg-3 hidden-xs body" >
-            <Ad position={"LEFT"} content={"sdfjslfjal sldjf"}/>
+            <div className="sticky col-sm-3 col-xs-3 col-md-3 col-lg-3">
+              <Search option={{}}/>
+              <div>
+                <Ad position={"LEFT"} content={"sdfjslfjal sldjf"}/>
+              </div>
+            </div>
             </div>
             <div className="col-sm-6 col-xs-6 col-md-6 col-lg-6 body">
-                <div className="contentImage">
-                </div>
-                <h3>THIS IS THE TITLEdsfsafsf asdfsafsa dfsdf</h3>
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
-                sldfjl alsldfjslafj asdlfj sadfljs asldfj sadflj asldfjsadlf alasjd flsdj fsladfj asldfjslda fasdlfj lasdjflsd fjlsdfj saldf asldfjsldfja sdfjsladfjlsadfj lsajdf sldflsdfj lsadfjlskdfjlksadfj lskdf jlskdfjsldkfj sal 
+                <Blogcontent/>
             </div>
             <div className="col-sm-3 col-xs-3 col-md-3 col-lg-3 hidden-xs body" >
-              <Ad position={"RIGHT"} content={"sdfdsafsf"}/>
+              <div className="sticky col-sm-3 col-xs-3 col-md-3 col-lg-3">
+                <div>
+                  <Ad position={"RIGHT"} content={"sdfdsafsf"}/>
+                </div>
+              </div>
             </div>
           </div>
         </div>
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Search extends Component {
+  render() {
+    return (
+        <div className="searchContainer">
+            <h5 className="Heading text-center">Search Filters</h5>
+        </div>
+    )
+  }
+}
