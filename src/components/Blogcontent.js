e3c55a686f2cf10d7e551fd4b0e1c39f7716aba8
senthur <senthur@financebuddha.com>
2018-05-07 01:12:07 +0530

    API Integrations

--- src/components/Blogcontent.js
+++ src/components/Blogcontent.js
@@ -1,37 +1,103 @@
-import React, { Component } from 'react';
-var trends=require('./../imports/trends');
+import React,{Component} from 'react';
+import './../styles/css/blog-contents.css';
+// var request = require("request");
+
 export default class Blogcontent extends Component {
-    componentWillMount(){
-        trends('India', 10, function(err, data) {
-            if (err) return console.err(err);
-            let topSites=[];
-            topSites=Object.keys(data).map((e)=>{
-            var singlesite={};
-                singlesite.title=(data[e]['title'].length) ? data[e]['title'][0] : "";
-                singlesite.description=data[e]['description'].length ? data[e]['description'][0] : "";
-                singlesite.traffic=data[e]['ht:approx_traffic'].length ? data[e]['ht:approx_traffic'][0].replace(/,/g,'').replace('+','') : "";
-                singlesite.contentLink=data[e]['ht:news_item'].length ? ((data[e]['ht:news_item'][0]['ht:news_item_url'].length) ? data[e]['ht:news_item'][0]['ht:news_item_url'][0] : "" ) : "";
-                singlesite.contentImageUrl=data[e]['ht:picture'].length ? data[e]['ht:picture'][0] : "";
-                singlesite.dateTracked=data[e]['pubDate'].length ? data[e]['pubDate'][0] : "";
-                topSites.push(singlesite);
-                return topSites;
-            });
+    constructor(){
+        super();
+        this.state={
+            contents_in_view:[]
+        }
+    }
+    sortByCity(cityname){
+        fetch('http://localhost:3001/sitesbycities/'+cityname,{headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }})
+        .then(response=> {return response.json()})
+        .then(data=>{
+            this.setState({contents_in_view : data});
+        }).catch(err=>{
+            console.log(err);
         });
+    }
+    componentWillMount(){
+        this.sortByCity('India');
     }
     render() {
         return (
             <div className="content-wrapper">
-                <h3 className="Heading">THIS IS THE TITLEdsfsafsf asdfsafsa dfsdf</h3>
-                <div className="contenttab">
-                </div>
-                <div className="contenttab">
-                </div>
-                <div className="contenttab">
-                </div>
-                <div className="contenttab">
-                </div>
-                <div className="contenttab">
-                </div>
+                <h3 className="Heading">Top Searches</h3>
+                
+                {    
+                    this.state.contents_in_view.map((e,v)=>{
+                        var current_classname="blog-card "+((v % 2!==0) ? "alt" : "");
+                        var current_imageURL={background: 'url("'+e.contentImageUrl+'") center no-repeat',backgroundSize: '100% 100%'};
+                        return (                
+                                <div className={current_classname} key={v}>
+                                    <div className="photo" style={current_imageURL}></div>
+                                    <ul className="details">
+                                        <li className="author"><a href="">John Doe</a></li>
+                                        <li className="date">Aug. 24, 2015</li>
+                                        <li className="tags">
+                                            <ul>
+                                                <li><a href="">Learn</a></li>
+                                                <li><a href="">Code</a></li>
+                                                <li><a href="">HTML</a></li>
+                                                <li><a href="">CSS</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                    <div className="description">
+                                        <h1>{e.title}<span style={{float:'right',fontSize:'12px'}}><span className="glyphicon glyphicon-eye-open"></span>{e.traffic}</span></h1>
+                                        <h2>{e.description}</h2>
+                                        <p className="summary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.
+                                        {/* {e.contentLink} */}
+                                        </p>
+                                        <a href={e.contentLink}>Read More</a>
+                                    </div>
+                                </div>
+                            )
+                    })
+                }
+                {/* <div class="blog-card alt">
+                    <div class="photo" style={{background: 'url("http://i62.tinypic.com/34oq4o0.jpg") center no-repeat',backgroundSize: 'cover'}}></div>
+                    <ul class="details">
+                        <li class="author"><a href="#">Jane Doe</a></li>
+                        <li class="date">July. 15, 2015</li>
+                        <li class="tags">
+                            <ul>
+                                <li><a href="#">Learn</a></li>
+                                <li><a href="#">Code</a></li>
+                                <li><a href="#">JavaScript</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div class="description">
+                        <h1>Mastering the Language</h1>
+                        <h2>Java is not the same as JavaScript</h2>
+                        <p class="summary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
+                        <a href="#">Read More</a>
+                    </div>
+                </div> */}
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                                {/* {e.title}
+                                {e.description}
+                                {e.traffic}
+                                {e.contentLink}
+                                {e.contentImageUrl}
+                                {e.dateTracked} */}
+                  
         </div>
         )
     }
