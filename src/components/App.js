0ef398d86bfebf8d77fe5e1841b28b323a3fb4bd
senthur <senthur@financebuddha.com>
2018-05-15 03:31:04 +0530

    COUNTRY SEARCH api done with incremental view more

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,34 @@ import Blogcontent from './Blogcontent';
 import './../styles/css/Main.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      filter : {
+        name:"country",
+        value:"India"
+      }
+    }
+    this.changefilter=this.changefilter.bind(this);
+  }
+
+  changefilter(filterdata){
+    console.log(filterdata);
+    switch(filterdata.name){
+      case "country":
+          this.setState({filter : filterdata})
+          break;
+      case "query":
+          break;
+      default:
+          break;
+    }
+  }
+
   componentDidMount() {
     document.title = "Amazing Page";
   }
+
   render() {
     return (
       <div>
@@ -21,14 +46,14 @@ class App extends Component {
           <div className='row contentroot'>
             <div className="col-sm-3 col-xs-3 col-md-3 col-lg-3 hidden-xs body searchoptions" >
             <div className="sticky col-sm-3 col-xs-3 col-md-3 col-lg-3">
-              <Search option={{}}/>
+              <Search filter={this.changefilter}/>
               <div>
                 <Ad position={"LEFT"} content={""}/>
               </div>
             </div>
             </div>
             <div className="col-sm-6 col-xs-6 col-md-6 col-lg-6 body">
-                <Blogcontent/>
+                <Blogcontent filter={this.state.filter}/>
             </div>
             <div className="col-sm-3 col-xs-3 col-md-3 col-lg-3 hidden-xs body" >
               <div className="sticky col-sm-3 col-xs-3 col-md-3 col-lg-3">
--- src/components/Blogcontent.js
+++ src/components/Blogcontent.js
@@ -1,56 +1,124 @@
 import React,{Component} from 'react';
-import './../styles/css/blog-contents.css';
+import './../styles/css/blog.css';
 // HTML Parser modules
 import Parser from 'html-react-parser';
 // import { render } from 'react-dom';
 ////////////////////////////////////
 
-
-
-
 export default class Blogcontent extends Component {
     constructor(){
         super();
         this.state={
+            error:false,
             loader:true,
-            contents_in_view:[]
+            contents_in_view:[],
+            resultcount:10,
+            next:false
         }
+        this.moreresults=this.moreresults.bind(this);
     }
     sortByCity(cityname){
-        fetch('https://adminsa.herokuapp.com/sitesbycities/'+cityname,{headers: {               //http://localhost:3001/
+        this.setState({error:false,loader:true,contents_in_view : []});
+        fetch('https://adminsa.herokuapp.com/sitesbycities/'+cityname+'/'+this.state.resultcount,{headers: {               //http://localhost:3001/
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }})
+        .then(response=> {return response.json()})
+        .then(data=>{
+            this.setState({error:false,loader:false,contents_in_view : data},()=>{this.checknextsets(cityname)});
+        }).catch(err=>{
+            this.setState({error:true,loader:false,contents_in_view : err});
+        });
+    }
+    checknextsets(cityname){
+        fetch('https://adminsa.herokuapp.com/sitesbycities/'+cityname+'/'+this.state.resultcount+10,{headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           }})
         .then(response=> {return response.json()})
         .then(data=>{
-            this.setState({loader:false,contents_in_view : data});
+            var previousstate=this.state;
+            if(data.length > this.state.contents_in_view.length){
+                previousstate.next=true;
+            }else{
+                previousstate.next=false;
+            }
+            this.setState(previousstate);
         }).catch(err=>{
-            console.log(err);
+            this.setState({error:true,loader:false,contents_in_view : err});
         });
     }
-    componentWillMount(){
-        this.sortByCity('India');
+
+    componentWillMount() {
+        this.applyfilter(this.props);
+    }
+    componentWillReceiveProps(nextProps) {
+        var previousstate=this.state;
+            previousstate.resultcount=10;
+            this.setState(previousstate);
+        this.applyfilter(nextProps)
+    }
+    applyfilter(props){
+        switch(props.filter.name){
+            case "country":
+                this.sortByCity(props.filter.value);
+                break;
+            case "query":
+                break;
+            default:
+                break;
+        }
+    }
+    moreresults(){
+        var previousstate=this.state;
+            previousstate.resultcount=this.state.resultcount+10;
+            previousstate.next=false;
+        this.setState(previousstate,()=>{
+            this.applyfilter(this.props);
+        });        
     }
     parseHTML(value){
         return Parser(value);
     }
+
+    createContentUrl(event,i){
+        event.preventDefault();
+        console.log(this.state.contents_in_view[i]);
+        // var media=event.target.parentElement.className;
+        // this.socialshare(media);    
+    }
+    socialshare(url,media){
+        switch(media){
+            case "social_media_f":
+                break;
+            case "social_media_t":
+                break;
+            case "social_media_i":
+                break;
+            case "social_media_w":
+                break;
+            default:
+                break;
+        }
+    }    
     render() {
         var renderelement= (
             <div className="content-wrapper">
-                <h1 className="Heading">India's Top Searches</h1>
+                <h1 className="Heading">Trending on {this.props.filter.value}</h1>
                 {    
                     (this.state.loader) ? <div className="loader-wrapper"><label className="loader"></label></div> : 
+                    (this.state.error) ? <div className="text-center error_info">Something Went Wrong. Please try again Later!!</div> :
                     this.state.contents_in_view.map((e,v)=>{
                         var current_classname="blog-card "+((v % 2!==0) ? "alt" : "");
-                        var current_imageURL={background: 'url("'+e.contentImageUrl+'") center no-repeat',backgroundSize: '100% 100%'};
+                        var current_imageURL={background: 'url("'+e.contentImageUrl+'") center no-repeat',backgroundSize: '150px 150px'};
                         return (                
                                 <div className={current_classname} key={v}>
                                     <div className="blog-picture">
                                         <div className="photo" style={current_imageURL}></div>
-                                        <div className="views">
-                                            <span className="glyphycon-icon view-icon"></span>
-                                            <span className="search_traffic">{e.traffic}+ <i>Views</i></span>
-                                        </div>
+                                    </div>
+                                    <div className="views">
+                                        <span className="glyphycon-icon view-icon"></span>
+                                        <span className="search_traffic">{e.traffic}+ <i>Views</i></span>
                                     </div>
                                     <ul className="details">
                                         <li className="author">Share</li>
@@ -58,10 +126,10 @@ export default class Blogcontent extends Component {
                                         <li className="tags"> */}
                                             {/* SHARE options  */}
                                             {/* <ul> */}
-                                                <li><a href=""><div className="social_media_f"></div></a></li>
-                                                <li><a href=""><div className="social_media_t"></div></a></li>
-                                                <li><a href=""><div className="social_media_i"></div></a></li>
-                                                <li><a href=""><div className="social_media_w"></div></a></li>
+                                                <li><div className="social_media_f"><a href="" onClick={(event)=>this.createContentUrl(event,v)} className="social-links"> </a></div></li>
+                                                <li><div className="social_media_t"><a href="" onClick={(event)=>this.createContentUrl(event,v)} className="social-links"> </a></div></li>
+                                                <li><div className="social_media_i"><a href="" onClick={(event)=>this.createContentUrl(event,v)} className="social-links"> </a></div></li>
+                                                <li><div className="social_media_w"><a href="" onClick={(event)=>this.createContentUrl(event,v)} className="social-links"> </a></div></li>
                                             {/* </ul> */}
                                         {/* </li> */}
                                     </ul>
@@ -83,6 +151,11 @@ export default class Blogcontent extends Component {
                             )
                     })
                 }               
+        { (!this.state.error) && (!this.state.loader) && (this.state.next) ? 
+            <div className="More-results">
+                <span className="highlight" onClick={this.moreresults}>View More</span>
+            </div> : ""
+        }
         </div>
         )
 
