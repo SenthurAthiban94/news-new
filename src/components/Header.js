f763382d007513bfa60f6210238c7470be973373
senthur <senthur@financebuddha.com>
2018-05-20 21:40:30 +0530

    homelink for logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component{
     render(){
         return(
             <div className="header_container">
-                <img src={this.props.logo_url} className='header_logo' alt="LOGO" />
+                <a href="" onClick={this.movetohome} ><img src={this.props.logo_url} className='header_logo' alt="LOGO" /></a>
                 <a href="" onClick={this.movetohome} ><img src={"./assets/images/home_icon.png"} style={{width:50,height:50,float:"right",margin:5}} alt="LOGO" /></a>
             </div>
         )
